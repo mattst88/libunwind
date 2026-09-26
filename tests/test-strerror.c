470f0b056c3c04cd9778e33adff72fa193eb3dcc
Matt Turner <mattst88@gmail.com>
2026-09-25 23:13:50 -0400

    DO NOT MERGE: tests: crash test-strerror to check core backtraces

--- tests/test-strerror.c
+++ tests/test-strerror.c
@@ -1,6 +1,16 @@
 #include "compiler.h"
 #include <libunwind.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/* DO NOT MERGE: crash inside libunwind, called from libc. */
+static int
+cmp (const void *a, const void *b)
+{
+  unw_word_t ip;
+  unw_get_reg (NULL, UNW_REG_IP, &ip);
+  return *(const int *) a - *(const int *) b;
+}
 
 int
 main (int argc, char **argv UNUSED)
@@ -14,5 +24,7 @@ main (int argc, char **argv UNUSED)
       if (verbose)
 	printf ("%6d -> %s\n", -i, msg);
     }
+  int v[] = { 2, 1 };
+  qsort (v, 2, sizeof v[0], cmp);
   return 0;
 }
